6ec41cbe6739f7af60b4538885b52e7e5e511600
Azzirr <arek.sliwowski37@gmail.com>
2023-02-15 10:19:56 +0100

    Cross off product funcionality

--- shopping-list/src/components/Product.js
+++ shopping-list/src/components/Product.js
@@ -2,20 +2,23 @@ import React, { useState } from 'react';
 import data from '../products';
 export default function Product(props){
     const {product, onAdd} = props;
-    const {style, setStyle} = useState()
     // TO DO! Delete item funcionality!
-    function deleteItem(event){
-        event.preventDefault()
-        setStyle({"text-decoration": `${product.name === "Beer" ? "line-through" : "auto" }`})
+    const [btnState, setBtnState] = useState(false);
+    const [off, setOff] = useState(false)
+    function handleClick(event){
+        event.preventDefault();
+        setBtnState(btnState => !btnState)
+        setOff(!off)
     }
+    let toggleClassCheck = btnState ? ' active' : null;
     return (
     <div>
         <div>
             <ul>
-                <h3 style={style}>{product.name}</h3>
+                <h3 className={`button${toggleClassCheck}`}>{product.name}</h3>
             </ul>
             <div>
-                <button onClick={() => onAdd(product)} onContextMenu={deleteItem}>Add to cart</button>
+                <button onClick={() => onAdd(product)} onContextMenu={handleClick} disabled={off}>Add to cart</button>
             </div>
         </div>
     </div>
